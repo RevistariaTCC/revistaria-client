b98ed5ab9f02500a84d53c9f63dfce3ef5a6b6d1
alan-pascke <alan.pascke@gmail.com>
2023-06-26 17:20:08 -0300

    ajustes na tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import Image from "next/image"
+import Logo from "../../public/images/logo.png"
+
 export default function Home() {
     return(
         <div className="grid grid-cols-2 h-screen grid-rows-4">
 
             <div className="bg-[#4C5A77] flex justify-center items-center cell:col-span-2 md:col-span-1 cell:row-span-1 md:row-span-4">
-                <div className="flex justify-center">
-                    image
+                <div className="p-4  min-w-[100px]">
+                    <Image src={Logo} alt="" className="mb-6"></Image>
+                    <div className="float-right text-2xl font-bold text-yellow-400">A Banca Moderna</div>
                 </div>
             </div>
 
             <div className="flex justify-center items-center border cell:col-span-2 md:col-span-1 cell:row-span-3 md:row-span-4">
                 <div>
-                <form className="bg-gray-200 shadow-md rounded-ss-[90px] rounded-ee-[90px] px-16 py-10 mb-4">
+                <form className="bg-gray-200 shadow-md rounded-ss-[90px] rounded-ee-[90px] cell:px-10 md:px-16 py-10 mb-4 border border-yellow-500">
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2">
                             Usuário
@@ -24,7 +28,7 @@ export default function Home() {
                         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                     </div>
                     <div className="flex items-center justify-between">
-                        <button className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Login
                         </button>
                         <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
