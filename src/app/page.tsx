2c32628dc1e35bc9f058b8d9936ecca0aa5941e7
Alan pascke <alan.pascke@gmail.com>
2023-06-28 01:46:58 -0300

    tela de login - fim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image"
 import Logo from "../../public/images/logo.png"
-import bancadaboca from "../../public/images/bancadaboca.jpg"
 
 export default function Home() {
     return(
         <div className="grid grid-cols-3 h-screen grid-rows-4">
 
-            <div className="bg-[#4C5A77] flex justify-center items-center cell:col-span-3 md:col-span-2 cell:row-span-1 md:row-span-4">
-                <div className="p-4 min-w-[100px]">
-                    <Image src={Logo} alt="" className="mb-6 md:w-96 cell:w-72"></Image>
+            <div className="bg-[#4C5A77] grid md:grid-rows-3  cell:col-span-3 md:col-span-2 cell:row-span-1 md:row-span-4">
+                <div></div>
+
+                <div className="grid justify-center p-4 min-w-[100px]">
+                    <Image src={Logo} alt="" className="cell:mb-0 xs:mb-6 md:w-96 cell:w-72"></Image>
                     <div className="grid justify-end">
                         <div className=" text-2xl font-bold text-white">A Banca de revistas</div>
-                        <div className="cell:text-2xl text-end md:text-5xl font-bold text-yellow-400 ">MODERNA</div>
+                        <div className="cell:text-2xl text-end md:text-5xl font-bold text-[#edd416] ">MODERNA</div>
                     </div>
                 </div>
+                
+                <div></div>
             </div>
 
             <div className="flex justify-center cell:mt-[10%] md:mt-0 md:items-center cell:col-span-3 md:col-span-1 cell:row-span-3 md:row-span-4">
                 <div> 
-                <form className="bg-gray-200 shadow-md rounded-ss-[90px] rounded-ee-[90px] cell:px-10 md:px-16 py-10 mb-4 border border-yellow-500">
+                <form className="bg-gray-200 shadow-md rounded-ss-[90px] rounded-ee-[90px] cell:px-10 md:px-16 py-10 mb-4 border border-yellow-500 grid grid-cols-1">
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2">
                             Usuário
@@ -32,7 +35,7 @@ export default function Home() {
                         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                     </div>
                     <div className="flex items-center justify-between">
-                        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 mr-2 rounded focus:outline-none focus:shadow-outline" type="button">
                             Login
                         </button>
                         <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
