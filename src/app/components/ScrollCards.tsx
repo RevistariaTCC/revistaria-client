254c05647d1263ff6b8cd6a208157eb322a81d70
samuelmeira <slmeira.dev@gmail.com>
2023-11-15 02:54:12 -0300

    fix: adjust carousel

--- src/app/components/ScrollCards.tsx
+++ src/app/components/ScrollCards.tsx
@@ -49,19 +49,48 @@ export default function ScrollCards(props: CarrouselProps) {
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
-  const NextArrowTemplate = (props: Settings) => {
+  const NextArrowTemplate = (props: any) => {
+    const {onClick,className, style } = props
     return (
-      <IconButton {...props}>
+      <IconButton onClick={onClick} className={className} style={style}>
         <ArrowForwardIosIcon color="action"/>
       </IconButton>
     );
   };
 
-  const PrevArrowTemplate = (props: Settings) => {
+  const PrevArrowTemplate = (props: any) => {
+    const {onClick, className, style } = props
     return (
-      <IconButton {...props}>
+      <IconButton onClick={onClick} className={className} style={style}>
         <ArrowBackIosIcon color="action"/>
       </IconButton>
     );
