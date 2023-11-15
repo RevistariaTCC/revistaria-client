ebe5c9f6e3e9cb08049fe7219fef2b62bf41333e
samuelmeira <slmeira.dev@gmail.com>
2023-11-15 02:19:18 -0300

    feat: finished volumes' carousel

--- src/app/components/ScrollCards.tsx
+++ src/app/components/ScrollCards.tsx
@@ -1,69 +1,116 @@
 import React, { useState } from "react";
-import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
 import VolumeModal from "./VolumeModal";
+import Slider, { Settings } from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 type CarrouselProps = {
   volumes: Volume[];
-}
+};
 
 type Volume = {
   id: number;
   title: string;
   category: string[];
   image: string;
-  status: 'AVAILABLE' | 'UNAVAILABLE';
+  status: "AVAILABLE" | "UNAVAILABLE";
   synopsis: string;
 };
 
+export default function ScrollCards(props: CarrouselProps) {
+  const [showModal, setShowModal] = useState(false);
 
-export default function ScrollCards(props : CarrouselProps) {
-
-  const [showModal,setShowModal] = useState(false);
-  
   const cards = props.volumes;
   const [activeVolume, setActiveVolume] = useState<Volume>({} as Volume);
-  const [volumesName, setVolumesName] = useState("");
 
   const DISPONIBILITY = {
-    AVAILABLE: {text: 'Disponivel', class: 'bg-green-400'},
-    UNAVAILABLE: {text: 'Indisponivel', class: 'bg-gray-300'}
-  }
+    AVAILABLE: { text: "Disponivel", class: "bg-green-400" },
+    UNAVAILABLE: { text: "Indisponivel", class: "bg-gray-300" },
+  };
 
-  const handleClick = (volume: Volume) =>{
+  const handleClick = (volume: Volume) => {
     setShowModal(true);
     setActiveVolume(volume);
-  }
+  };
+
+  const config = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
+  const NextArrowTemplate = (props: Settings) => {
+    return (
+      <IconButton {...props}>
+        <ArrowForwardIosIcon color="action"/>
+      </IconButton>
+    );
+  };
+
+  const PrevArrowTemplate = (props: Settings) => {
+    return (
+      <IconButton {...props}>
+        <ArrowBackIosIcon color="action"/>
+      </IconButton>
+    );
+  };
 
   return (
-    <div className="flex overflow-x-auto scrollbar">
-      {/* <div className="flex" onWheel={handleWheel} >
-        <button className="flex-none z-10 border-none bg-transparent rounded" onClick={showPrev}>
-          <ArrowBackIos/>
-        </button>  
-        <button className="flex-none z-10 border-none bg-transparent rounded" onClick={showNext}>
-          <ArrowForwardIos/>
-        </button>
-      </div> */}
-        <div className={`gap-2 z-0 flex p-2`}>
-          {cards.map((volume) => (
-              <Card key={volume.id} className='w-[130px] p-2 flex flex-col justify-center hover:-translate-y-2 transition ease-in-out duration-200 cursor-pointer'>
-                  <CardActionArea onClick={()=>handleClick(volume)} className='p-0 m-0 h-full'>
-                      <CardContent sx={{maxWidth: '100%', padding:'0'}}>
-                          <CardMedia
-                              component='img'
-                              image={volume.image}
-                              height={200}  
-                              className='my-1 max-w-full rounded'
-                          /> 
-                          <div className={`flex justify-center items-center rounded h-S8 ${DISPONIBILITY[volume.status].class}`}>
-                            <h4>{DISPONIBILITY[volume.status].text}</h4>
-                          </div>
-                      </CardContent>
-                  </CardActionArea>
-              </Card> 
-            ))}
-        </div>
-      <VolumeModal openModal={showModal} volumesName={activeVolume.title} synopsis={activeVolume.synopsis} handleClose={()=>setShowModal(false)}/>
-    </div>
-    )
-};
+    <>
+      <Slider
+        {...config}
+        adaptiveHeight
+        variableWidth
+        nextArrow={<NextArrowTemplate />}
+        prevArrow={<PrevArrowTemplate />}
+      >
+        {cards.map((volume) => (
+          <Card
+            key={volume.id}
+            className="p-2 max-w-60 hover:-translate-y-2 transition ease-in-out duration-200 cursor-pointer"
+          >
+            <CardActionArea
+              onClick={() => handleClick(volume)}
+              className="p-0 m-0 h-full"
+            >
+              <CardContent sx={{ maxWidth: "100%", padding: "0" }}>
+                <CardMedia
+                  component="img"
+                  image={volume.image}
+                  height={200}
+                  className="my-1 max-w-full rounded"
+                />
+                <div
+                  className={`flex justify-center items-center rounded h-S8 ${
+                    DISPONIBILITY[volume.status].class
+                  }`}
+                >
+                  <h4>{DISPONIBILITY[volume.status].text}</h4>
+                </div>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Slider>
+
+      <VolumeModal
+        openModal={showModal}
+        volumesName={activeVolume.title}
+        synopsis={activeVolume.synopsis}
+        handleClose={() => setShowModal(false)}
+      />
+    </>
+  );
+}
