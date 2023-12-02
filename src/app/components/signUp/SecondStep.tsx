4423069ab40952d912a7e2c11cc1b3b64fe21095
samuelmeira <slmeira.dev@gmail.com>
2023-12-02 03:13:08 -0300

    finished whatsapp integration

--- src/app/components/signUp/SecondStep.tsx
+++ src/app/components/signUp/SecondStep.tsx
@@ -10,8 +10,10 @@ import { useFormContext } from "react-hook-form";
 import { PinInput } from "./PinInput";
 import { useCallback, useEffect, useState } from "react";
 import { useMutation } from "react-query";
-import { validateCode } from "@/services/api/internal/code";
+import { generateCode, validateCode } from "@/services/api/internal/code";
 import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { useTimer } from "react-timer-hook";
 
 interface iSecondStep {
   next(): void;
@@ -22,8 +24,20 @@ export default function SecondStep({ next, previous }: iSecondStep) {
   const { getValues } = useFormContext();
 
   const [activationCode, setActivationCode] = useState("");
+  const [hideTimer, setHideTimer] = useState(false);
 
   const validateCodeMutation = useMutation(validateCode);
+
+  const generateTime = () => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 15);
+    return time;
+  };
+
+  const { restart, seconds } = useTimer({
+    expiryTimestamp: generateTime(),
+    onExpire: () => setHideTimer(true),
+  });
 
   const isValid = useCallback(() => {
     return activationCode.trim().length === 6;
@@ -37,6 +51,15 @@ export default function SecondStep({ next, previous }: iSecondStep) {
       phone: getValues("phone"),
     });
   }, [isValid]);
+
+  const generateCodeMutation = useMutation(generateCode);
+
+  const handleResendCode = () => {
+    restart(generateTime());
+    setHideTimer(false);
+
+    generateCodeMutation.mutate(getValues("phone"));
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -70,16 +93,48 @@ export default function SecondStep({ next, previous }: iSecondStep) {
               <br /> Por favor revise o telefone e o código informados!
             </Typography>
           )}
-          <Button
-            onClick={next}
-            disabled={!isValid() || validateCodeMutation.isError}
-            fullWidth
-            autoFocus={false}
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Confirmar
-          </Button>
+          <div className="flex justify-center items-center">
+            <Button
+              onClick={handleResendCode}
+              variant="contained"
+              className="bg-white text-indigo-800 w-48 h-10 hover:bg-blue-600 hover:text-white"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={!hideTimer}
+            >
+              {hideTimer ? (
+                <Typography className="flex flex-col text-xs justify-center">
+                  Reenviar código
+                </Typography>
+              ) : (
+                <Typography className="flex flex-col text-xs justify-center">
+                  <RestoreIcon sx={{ width: 20 }} />
+                  {seconds}
+                </Typography>
+              )}
+            </Button>
+          </div>
+          <div className="flex justify-center items-center gap-6">
+            <Button
+              onClick={previous}
+              variant="contained"
+              className="bg-white text-indigo-800 w-full h-10 hover:bg-blue-600 hover:text-white"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              <Typography className="flex flex-col text-xs justify-center">
+                Voltar
+              </Typography>
+            </Button>
+            <Button
+              onClick={next}
+              disabled={!isValid() || validateCodeMutation.isError}
+              fullWidth
+              variant="contained"
+              className="py-2"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Confirmar
+            </Button>
+          </div>
         </Box>
       </Box>
     </Container>
