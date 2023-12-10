6a8653c3665c98660321cb0b5202adcf8c1b97c2
Samuel Meira <slmeira.dev@gmail.com>
2023-12-10 16:58:30 -0300

    refactor: change endpoint body

--- src/app/components/userModal/SignIn.tsx
+++ src/app/components/userModal/SignIn.tsx
@@ -9,10 +9,11 @@ import { useMutation } from "react-query";
 import { createSession } from "@/services/api/internal/session";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { SignInType } from "@/schemas/UserSignIn";
-import { LinearProgress, TextField } from "@mui/material";
+import { Alert, LinearProgress, Snackbar, TextField } from "@mui/material";
 import { useAuth } from "@/hooks/auth";
 import { IMaskInput } from "react-imask";
 import React, { useState } from "react";
+import RecoveryPasswordComponent from "./RevoreyPassword";
 
 interface CustomProps {
   onChange: (event: { target: { name: string; value: string } }) => void;
@@ -21,6 +22,18 @@ interface CustomProps {
 }
 
 export default function SignIn() {
+  const [openAlert, setOpenAlert] = useState(false);
+
+  const handleCloseAlert = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenAlert(false);
+  };
   const [showUserModal, setShowUserModal] = useState({ open: false, type: "" });
   const { handleSubmit, register } = useForm<SignInType>({
     defaultValues: {
@@ -28,6 +41,8 @@ export default function SignIn() {
       password: "",
     },
   });
+
+  const [recoveryPassword, setRecoveryPassword] = useState(false);
   const { signIn } = useAuth();
   const loginMutation = useMutation(createSession, {
     onSuccess: (data) => {
@@ -36,17 +51,9 @@ export default function SignIn() {
   });
 
   const onSubmit: SubmitHandler<SignInType> = (data) => {
-    loginMutation.mutate({...data, cpf: data.cpf.replaceAll(/[^0-9]+/g, '')});
+    loginMutation.mutate({ ...data, cpf: data.cpf.replaceAll(/[^0-9]+/g, "") });
   };
 
-  const openUserModal = (type: string) => {
-    setShowUserModal({ open: true, type });
-  };
-
-  const closeUserModal = () =>{
-    setShowUserModal({open: false, type:'userModal'})
-    showUserModal
-  }
 
   const TextMaskCustom = React.forwardRef<HTMLInputElement, CustomProps>(
     function TextMaskCustom(props, ref) {
@@ -65,6 +72,11 @@ export default function SignIn() {
     }
   );
 
+  if (recoveryPassword)
+    return (
+      <RecoveryPasswordComponent onUpdate={() => {setRecoveryPassword(false); setOpenAlert(true)}} />
+    );
+
   return (
     <Container component="main" maxWidth="xs">
       {loginMutation.isLoading && <LinearProgress />}
@@ -76,7 +88,7 @@ export default function SignIn() {
           flexDirection: "column",
           alignItems: "center",
         }}
-        >
+      >
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
@@ -88,7 +100,7 @@ export default function SignIn() {
           onSubmit={handleSubmit(onSubmit)}
           noValidate
           sx={{ mt: 1 }}
-          >
+        >
           <TextField
             {...register("cpf")}
             margin="normal"
@@ -127,15 +139,30 @@ export default function SignIn() {
           </Button>
         </Box>
       </Box>
-        <button 
-          type="button"
-          className='outline-none border-none bg-transparent text-blue-800 hover:text-blue-500 text-lg cursor-pointer'
-          onClick={()=>{
-            closeUserModal();
-          }}
-          >
-            Esqueci a senha
-        </button>
+      <button
+        type="button"
+        className="outline-none border-none bg-transparent text-blue-800 hover:text-blue-500 text-sm cursor-pointer flex w-full justify-end"
+        onClick={() => {
+          setRecoveryPassword(true);
+        }}
+      >
+        Esqueceu a senha?
+      </button>
+      <Snackbar
+        className="fixed"
+        open={openAlert}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert
+          onClose={handleCloseAlert}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Senha atualizada com sucesso!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
